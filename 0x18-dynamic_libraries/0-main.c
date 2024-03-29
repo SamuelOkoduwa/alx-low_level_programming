65d35033d94ac011b401d81da391d2ec121ad418
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-03-29 12:14:21 +0100

    The dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,25 @@
+/* 1-putchar.c */
+#include "main.h"
+
+int _putchar(char c) {
+    /* Placeholder implementation */
+    return 0;
+}
+
+/* 2-islower.c */
+#include "main.h"
+
+int _islower(int c) {
+    /* Placeholder implementation */
+    return 0;
+}
+
+/* 3-isalpha.c */
+#include "main.h"
+
+int _isalpha(int c) {
+    /* Placeholder implementation */
+    return 0;
+}
+
+/* Other functions with placeholder implementations */
