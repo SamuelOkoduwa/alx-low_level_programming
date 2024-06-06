8635d6c45e6ba58998f66ee84651661d60ae6002
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-06 06:16:16 -0700

    First make file

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+// main.c
+#include <stdio.h>
+
+// Function prototype from school.c
+void print_school();
+
+int main(void)
+{
+    printf("This is the main program.\n");
+    print_school(); // Call the function defined in school.c
+    return 0;
+}
+
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,8 @@
+// school.c
+#include <stdio.h>
+
+void print_school()
+{
+    printf("Welcome to the school program!\n");
+}
+
