2f3433f5c528f2e99a3566764699d35f5cab8a73
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-06 14:09:09 -0700

    a function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+    
+    Parameters:
+    grid (list of list of int): 2D list representing the grid where
+                                0 is water and 1 is land.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == rows - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == cols - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+                    
+    return perimeter
+
